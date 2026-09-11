56c2eb44b0f19df50a7f42a0675dc0990b444bc3
Siddharht-dev <siddharthwork1719@gmail.com>
2026-09-11 22:33:39 +0530

    Working on frontend

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 require("dotenv").config();
 const app = express();
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 
 mongoose.connect(process.env.MONGO_URI)
@@ -14,6 +15,7 @@ mongoose.connect(process.env.MONGO_URI)
         console.log("MongoDB connection failed: ",err);
     })
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/jobs", jobRoutes);
